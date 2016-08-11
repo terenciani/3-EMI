0c753c2ec6092fe77f8ad0ceca4e19dbde32b061
Marcelo Terenciani <marcelo.figterenciani@gmail.com>
2016-08-11 08:46:31 -0300

    conexao com o banco de dados sem verificar se esta funcionando

--- LTDP_3EMI/Projeto-feira/salvarinscricao.php
+++ LTDP_3EMI/Projeto-feira/salvarinscricao.php
@@ -1,6 +1,19 @@
 <?php
-
-	var_dump($_POST);
-	echo $_POST['nome'];
-
+	echo var_dump($_GET) . "GET <br>";
+	echo var_dump($_POST) . "POST <br>";
+	echo "posicao nome" . $_POST['nome'];
+	
+	
+	/*configurco de enderecmento do bnco de ddos*/
+	$servidor = "localhost";
+	$usuario = "root";
+	$senha = "";
+	
+	/*configurco de cesso o bnco de ddos*/
+	$nome_banco = "bd_centro_interesse";
+	
+	$conexao = mysql_connect($servidor, $usurio, $senha);
+	
+	$banco = mysql_select_db($nome_banco, $conexao);
+	
 ?>
